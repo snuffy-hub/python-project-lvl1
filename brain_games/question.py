06b746e3a5bbbd609e064c360ee16452e49725df
anton_termux <belyak0vap@yandex.ru>
2021-04-09 15:06:05 +0500

    brain-even game (linter correction)

--- brain_games/question.py
+++ brain_games/question.py
@@ -10,15 +10,17 @@ def welcome_user():
 
 def ask_user():
     name = welcome_user()
-    print(f'Answer "yes" if the number is even, otherwise answer "no".')
+    print('Answer "yes" if the number is even, otherwise answer "no".')
     count = 0
     while count < 3:
         correct_answer = random.randint(0, 100)
-        user_answer = prompt.string(f'Question: {correct_answer}\nYour answer: ')
+        user_answer = prompt.string(f'Question:{correct_answer}\nYour answer:')
+        print('')
         if int(user_answer) == correct_answer:
             print('Correct!')
             count += 1
         else:
-            print(f"'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, {name}!")
+            print("'yes' is wrong answer ;(. Correct answer was 'no'.")
+            print(f'Let\'s try again, {name}!')
             count = 0
     print(f'Congratulations, {name}!')
